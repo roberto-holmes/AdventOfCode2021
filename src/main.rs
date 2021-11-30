1576b0dbd708ae2114583cd456e5a93902b087ae
Roberto <robertoholmes@live.com>
2021-11-30 13:58:29 +0100

    Prepared project for advent of code by accepting command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod day0;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Collect command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Get the day number to be run from the arguments
+    let current_day: i32 = args[1].parse().unwrap();
+
+    // Run the relevant day
+    match current_day {
+        0 => day0::run(),
+        _ => println!("Invalid day"),
+    }
 }
