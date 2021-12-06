c17696a45060ff5af5c0026cc0adbcd058d7f550
Roberto <robertoholmes@live.com>
2021-12-06 12:30:43 +0100

    Completed Day 6 Part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use std::{fs, io};
 
 pub fn run() {
@@ -9,7 +10,7 @@ pub fn run() {
     let input: Vec<&str> = input.split(",").collect();
 
     println!("Part 1 is {:?}", part1(&input, 80));
-    // println!("Part 2 is {:?}", part2());
+    println!("Part 2 is {:?}", part2(&input, 256));
 }
 
 fn part1(input: &Vec<&str>, days: u32) -> Result<i32, io::Error> {
@@ -37,4 +38,45 @@ fn part1(input: &Vec<&str>, days: u32) -> Result<i32, io::Error> {
     }
 
     Ok(fish.len() as i32)
+}
+
+fn part2(input: &Vec<&str>, days: u32) -> Result<u64, io::Error> {
+    // Store fish in array by using tehir timer as an index
+    let mut fish = [0 as u64; 9];
+    // Initialise array
+    for i in input {
+        fish[i.parse::<usize>().unwrap()] += 1;
+    }
+
+    let mut fish_count = 0;
+
+    // Cycle through each day
+    for d in 0..days {
+        // Start a timer for benchmarking
+        let start = Instant::now();
+        // Store how many new fish will be created
+        let new_fish = fish[0];
+        // Cycle through each fish and lower their timer by 1
+        for f in 1..fish.len() {
+            fish[f - 1] = fish[f];
+        }
+        // Create the new fish and move the parents to a timer of 8
+        fish[6] += new_fish;
+        fish[8] = new_fish;
+        let duration = start.elapsed();
+
+        // Count the fish
+        fish_count = 0;
+        for f in &fish {
+            fish_count += f;
+        }
+
+        // Print debug information
+        println!(
+            "Day {} has a length of {} and took {:?}",
+            d, fish_count, duration
+        );
+    }
+
+    Ok(fish_count)
 }
