1ab8875b9b500c694c59b5b5e8437aa9dfd955b1
Roberto <robertoholmes@live.com>
2021-12-07 11:08:17 +0100

    Completed Day 7 Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use std::{fs, io};
 
 pub fn run() {
@@ -14,7 +15,7 @@ pub fn run() {
     }
 
     println!("Part 1 is {:?}", part1(&input_num));
-    // println!("Part 2 is {:?}", part2(&input_num));
+    println!("Part 2 is {:?}", part2(&input_num));
 }
 
 fn part1(input: &Vec<u32>) -> Result<i32, io::Error> {
@@ -34,6 +35,41 @@ fn part1(input: &Vec<u32>) -> Result<i32, io::Error> {
             best_pos = pos;
         }
     }
+    println!("Optimal alignment position is {}", best_pos);
+    Ok(best_cost)
+}
+
+fn part2(input: &Vec<u32>) -> Result<i32, io::Error> {
+    // Create values for storing the current best
+    let mut best_pos = 0;
+    let mut best_cost = 0;
+    let mut last_cost = 0;
+    // Start a timer for benchmarking
+    let start = Instant::now();
+    // Cycle through each potential position
+    for pos in 0..input.len() {
+        let mut cost: i32 = 0;
+        // Calculate the cost if each value were to go to that position
+        for i in input {
+            // Implement the new cost function
+            for step in 0..(*i as i32 - pos as i32).abs() + 1 {
+                cost += step;
+            }
+        }
+        // println!("Position {} has a cost of {}", pos, cost);
+        // If the cost is a personal best, store it
+        if best_cost == 0 || cost < best_cost {
+            best_cost = cost;
+            best_pos = pos;
+        }
+        // Avoid unecessary calculation if we already have the answer
+        if cost < last_cost || last_cost == 0 {
+            last_cost = cost
+        } else {
+            break;
+        }
+    }
+    println!("Part 2 completed in {:?}", start.elapsed());
     println!("Optimal alignment position is {}", best_pos);
     Ok(best_cost)
 }
