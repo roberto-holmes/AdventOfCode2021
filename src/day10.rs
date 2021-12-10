66392db22cf86d1d855d22d96bc5573e40e3986e
Roberto <robertoholmes@live.com>
2021-12-10 12:42:17 +0100

    Completed Day 10 Part 2

--- src/day10.rs
+++ src/day10.rs
@@ -10,7 +10,7 @@ pub fn run() {
     let input: Vec<&str> = input.split("\r\n").collect();
 
     println!("Part 1 is {:?}", part1(&input));
-    // println!("Part 2 is {:?}", part2(&input));
+    println!("Part 2 is {:?}", part2(&input));
 }
 
 fn part1(input: &Vec<&str>) -> Result<u32, io::Error> {
@@ -68,4 +68,84 @@ fn part1(input: &Vec<&str>) -> Result<u32, io::Error> {
         println!();
     }
     Ok(answer)
+}
+
+fn part2(input: &Vec<&str>) -> Result<u64, io::Error> {
+    let mut answer: Vec<u64> = Vec::new();
+    for line_num in 0..input.len() {
+        answer.push(0);
+        let line = input[line_num];
+        // Store active chunks
+        let mut brackets = Vec::new();
+        let line_chars: Vec<char> = line.chars().collect();
+        for i in 0..line_chars.len() {
+            let c = line_chars[i];
+            // Check if a chunk is starting
+            if c == '(' || c == '[' || c == '{' || c == '<' {
+                // Add it to active chunks
+                brackets.push(c);
+                print!("{}", c.to_string().blue());
+            } else {
+                // Check if the chunk is being closed with the right character
+                print!("{}", c.to_string().yellow());
+                let last = *brackets.last().unwrap();
+                if c == ')' {
+                    // If it is a valid
+                    if last == '(' {
+                        // Close the chunk by removing the opening character
+                        brackets.pop();
+                    } else {
+                        // Corrupt line
+                        print!(" corrupted line");
+                        break;
+                    }
+                } else if c == ']' {
+                    if last == '[' {
+                        brackets.pop();
+                    } else {
+                        // Corrupt line
+                        print!(" corrupted line");
+                        break;
+                    }
+                } else if c == '}' {
+                    if last == '{' {
+                        brackets.pop();
+                    } else {
+                        // Corrupt line
+                        print!(" corrupted line");
+                        break;
+                    }
+                } else if c == '>' {
+                    if last == '<' {
+                        brackets.pop();
+                    } else {
+                        // Corrupt line
+                        print!(" corrupted line");
+                        break;
+                    }
+                } else {
+                    panic!("{} is an unexpected character", c);
+                }
+            }
+            // If the end of the line is reached and there are still active chunks
+            if i == line_chars.len() - 1 && brackets.len() != 0 {
+                // Calculate the cost of closing each chunk
+                for j in (0..brackets.len()).rev() {
+                    answer[line_num] *= 5;
+                    match brackets[j] {
+                        '(' => answer[line_num] += 1,
+                        '[' => answer[line_num] += 2,
+                        '{' => answer[line_num] += 3,
+                        '<' => answer[line_num] += 4,
+                        _ => panic!("Invalid character in brackets vector"),
+                    }
+                }
+            }
+        }
+        println!();
+    }
+    // Process the answer by getting the median non zero value
+    answer.retain(|&x| x != 0);
+    answer.sort();
+    Ok(answer[answer.len() / 2])
 }
