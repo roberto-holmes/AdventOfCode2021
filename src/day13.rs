eee638010c092bda96cf8668c7c3461ac1974fe1
Roberto <robertoholmes@live.com>
2021-12-13 18:04:38 +0100

    Completed Day 13 Part 2

--- src/day13.rs
+++ src/day13.rs
@@ -12,12 +12,12 @@ pub fn run() {
     println!("Day 13");
 
     // Open file
-    let input = fs::read_to_string("inputs/day13ex.txt").expect("File not found");
+    let input = fs::read_to_string("inputs/day13.txt").expect("File not found");
     // Extract each line
     let input: Vec<&str> = input.split("\r\n").collect();
 
-    println!("Part 1 is {:?}", part1(&input));
-    // println!("Part 2 is {:?}", part2(&input));
+    // println!("Part 1 is {:?}", part1(&input));
+    println!("Part 2 is {:?}", part2(&input));
 }
 
 fn part1(input: &Vec<&str>) -> Result<u32, io::Error> {
@@ -96,5 +96,84 @@ fn part1(input: &Vec<&str>) -> Result<u32, io::Error> {
     }
     // println!("{:?}", page);
     // println!("{:?}", instructions);
+    Ok(dot_count)
+}
+
+fn part2(input: &Vec<&str>) -> Result<u32, io::Error> {
+    let mut instructions = Vec::new();
+    let mut reached_instructions = false;
+    let mut page = Vec::new();
+    for y in 0..input.len() {
+        let row = input[y];
+        // Parse instructions
+        if reached_instructions {
+            let command: Vec<&str> = row.split(" ").collect();
+            let line: Vec<&str> = command[2].split("=").collect();
+            instructions.push(Fold {
+                direction: line[0].chars().next().unwrap(),
+                position: line[1].parse::<usize>().unwrap(),
+            });
+        }
+        // Figure out when the second section of the input has been reached
+        else if row == "" {
+            reached_instructions = true;
+        }
+        // Deal with dots on the page
+        else {
+            let command: Vec<&str> = row.split(",").collect();
+            let x = command[0].parse::<usize>().unwrap();
+            let y = command[1].parse::<usize>().unwrap();
+
+            // Add blank spaces up to the y coordinate
+            while page.len() <= y {
+                page.push(Vec::new());
+            }
+            // Add blank spaces up to the x
+            while page[y].len() <= x {
+                page[y].push(0);
+            }
+            // Add the dot
+            page[y][x] += 1;
+        }
+    }
+
+    // Perform folds
+    for instruction in instructions {
+        if instruction.direction == 'x' {
+            for y in 0..page.len() {
+                for x in (instruction.position..page[y].len()).rev() {
+                    page[y][2 * instruction.position - x] += page[y][x];
+                    // Remove part of the page that has een folded over
+                    page[y].remove(x);
+                }
+            }
+        } else if instruction.direction == 'y' {
+            for y in (instruction.position..page.len()).rev() {
+                for x in 0..page[y].len() {
+                    // Add extra blank x if they are needed
+                    while page[2 * instruction.position - y].len() <= x {
+                        page[2 * instruction.position - y].push(0);
+                    }
+                    page[2 * instruction.position - y][x] += page[y][x];
+                }
+                // Remove part of the page that has een folded over
+                page.remove(y);
+            }
+        }
+    }
+
+    // Count (and display) dots
+    let mut dot_count = 0;
+    for y in &page {
+        for x in y {
+            if x > &0 {
+                print!("{}", "#".yellow());
+                dot_count += 1;
+            } else {
+                print!("{}", "-".blue());
+            }
+        }
+        println!();
+    }
     Ok(dot_count)
 }
